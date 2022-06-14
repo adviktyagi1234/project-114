21315a8696b412ab80fc099ccc2a092641ab2ab2
adviktyagi1234 <92938197+adviktyagi1234@users.noreply.github.com>
2022-06-14 11:09:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload()
+{}
+
+function setup()
+{
+    canvas = create.canvas(450, 450)
+    canvas.center()
+}
+
+function draw()
+{}
+
+function takeSnapshot()
+{
+    save('pictureeeeeee')
+}
